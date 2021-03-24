19b0a786cc9c0f84ff019a358de6dc8dff8437e8
Alexis Saettler <alexis@saettler.org>
2021-03-24 23:11:17 +0100

    fix: fix checked (#17)

--- .eslintrc.js
+++ .eslintrc.js
@@ -75,6 +75,7 @@ module.exports = {
             }
         ],
 
+        "vue/no-v-model-argument": 1,
         "vue/no-v-html" : 0,
         "vue/max-attributes-per-line": [
             // https://vuejs.org/v2/style-guide/#Multi-attribute-elements-strongly-recommended
