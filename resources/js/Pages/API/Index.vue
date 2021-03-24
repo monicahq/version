393ceae96fdfbdd6572cbe473653a7586989b4b4
Alexis Saettler <alexis@saettler.org>
2021-03-24 20:45:26 +0100

    ci: add lint (#13)

--- resources/js/Pages/API/Index.vue
+++ resources/js/Pages/API/Index.vue
@@ -1,35 +1,36 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                API Tokens
-            </h2>
-        </template>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        API Tokens
+      </h2>
+    </template>
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <api-token-manager :tokens="tokens"
-                                   :available-permissions="availablePermissions"
-                                   :default-permissions="defaultPermissions" />
-            </div>
-        </div>
-    </app-layout>
+    <div>
+      <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <api-token-manager :tokens="tokens"
+                           :available-permissions="availablePermissions"
+                           :default-permissions="defaultPermissions"
+        />
+      </div>
+    </div>
+  </app-layout>
 </template>
 
 <script>
-    import ApiTokenManager from './ApiTokenManager'
-    import AppLayout from '@/Layouts/AppLayout'
+import ApiTokenManager from './ApiTokenManager';
+import AppLayout from '@/Layouts/AppLayout';
 
-    export default {
-        props: [
-            'tokens',
-            'availablePermissions',
-            'defaultPermissions',
-        ],
+export default {
 
-        components: {
-            ApiTokenManager,
-            AppLayout,
-        },
-    }
+  components: {
+    ApiTokenManager,
+    AppLayout,
+  },
+  props: [
+    'tokens',
+    'availablePermissions',
+    'defaultPermissions',
+  ],
+};
 </script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -8,14 +8,14 @@ import { InertiaProgress } from '@inertiajs/progress';
 const el = document.getElementById('app');
 
 createApp({
-    render: () =>
-        h(InertiaApp, {
-            initialPage: JSON.parse(el.dataset.page),
-            resolveComponent: (name) => require(`./Pages/${name}`).default,
-        }),
+  render: () =>
+    h(InertiaApp, {
+      initialPage: JSON.parse(el.dataset.page),
+      resolveComponent: (name) => require(`./Pages/${name}`).default,
+    }),
 })
-    .mixin({ methods: { route } })
-    .use(InertiaPlugin)
-    .mount(el);
+  .mixin({ methods: { route } })
+  .use(InertiaPlugin)
+  .mount(el);
 
 InertiaProgress.init({ color: '#4B5563' });
