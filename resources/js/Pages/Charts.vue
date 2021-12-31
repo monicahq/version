0a600bfc6fc484d403e31689aab776a078a9b4d2
Alexis Saettler <alexis@saettler.org>
2021-12-31 20:58:59 +0100

    fix

--- resources/js/Pages/Charts.vue
+++ resources/js/Pages/Charts.vue
@@ -1,7 +1,9 @@
 <template>
   <app-layout>
     <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Charts</h2>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Charts
+      </h2>
       <a
         href="#"
         class="text-sm text-gray-700 underline"
