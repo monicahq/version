d841d7dd4592d378e9e29367e41a23942a5a0f17
Alexis Saettler <alexis@saettler.org>
2022-01-09 22:48:26 +0100

    fix: crons last_run is nullable

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+use Illuminate\Database\DBAL\TimestampType;
 
 return [
 
@@ -156,4 +157,9 @@ return [
 
     ],
 
+    'dbal' => [
+        'types' => [
+            'timestamp' => TimestampType::class,
+        ],
+    ],
 ];
